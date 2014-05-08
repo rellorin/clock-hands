fc4d176fe0de31a82ab4af703734863527c2cd6f
ryanellorin <ryan.ellorin@gmail.com>
2014-05-07 21:34:08 -0400

    first commit

---
+++ clockhands.cpp
@@ -0,0 +1,24 @@
+//******************************************************************
+//Name: Ryan Ellorin
+//Project: Project 1 - Clock Hands
+//******************************************************************
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    int H;
+    int M;
+    double degrees;
+    
+    //User Prompt
+    cout<<"Enter the Hour.";
+    cin>>H;
+    cout<<"Enter the minutes.";
+    cin>>M;
+    
+    degrees = 360 - (((30*H)+(M/2))-6*M);
+    cout<<degrees;
+    cout<<endl;
+    system("PAUSE");
+}//end program
